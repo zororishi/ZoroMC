c811efa98b630087512bffc1bce3a05595efff51
zoro <rishitab2009@gmail.com>
2023-12-24 14:32:49 +0530

    idk

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 /* GET home page. */
-app.get('/', function(req, res, next) {
+app.get('/', function(req, res) {
     res.render('index')
   });
   app.use(express.static(path.join(__dirname, 'public')));
-
-  app.listen(8080)
+  app.listen(8080, ()=>{
+    console.log("started app")
+  })
 
 
   module.exports= app;
